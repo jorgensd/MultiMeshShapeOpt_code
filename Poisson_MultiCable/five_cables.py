16dbfb992c92a2d3606612749a647224f4f7296f
dokken <dokken92@gmail.com>
2018-10-03 12:58:33 +0200

    Satisfied with results in five cables and check if tolerance affects convergence

--- Poisson_MultiCable/five_cables.py
+++ Poisson_MultiCable/five_cables.py
@@ -1,18 +1,18 @@
 import numpy
 
-lmb_metal = [312.,429,205,205,205]
-lmb_insulation = [0.25,0.12,0.06,0.04,0.02]
+lmb_metal = [429,312,205,205,205]
+lmb_insulation = [0.03,0.12,0.06,0.04,0.02]
 lmb_fill = 0.33 
-c1 = numpy.array([0, 0.8])
+c1 = numpy.array([0, 0.6])
 c2 = numpy.array([-0.4, 0.2])
 c3 = numpy.array([-0.1, -0.4])
-c4 = numpy.array([0.52, 0.3])
+c4 = numpy.array([0.6, 0.4])
 c5 = numpy.array([0.45, -0.45])
 
 cable_positions = numpy.array([c1[0],c1[1],c2[0],c2[1],c3[0],c3[1],
                                c4[0],c4[1],c5[0],c5[1]])
 
-scales = numpy.array([0.75,1,0.9,1,0.8])   
+scales = numpy.array([1,0.75,0.9,1,0.8])   
 sources = numpy.array([1,0.5,0.25,0.125,0.0625])
 
 from MultiCable import *
@@ -29,16 +29,14 @@ for i in range(MC.multimesh.num_parts()):
 
 n_cables = MC.multimesh.num_parts()-1
 opt = MultiCableOptimization(n_cables, scales, MC.eval_J, MC.eval_dJ)
-# opt.nlp.num_option('obj_scaling_factor',1e-1) 
-opt.nlp.int_option('max_iter', 25)
-# opt.nlp.num_option('acceptable_tol', 1e-3)
-opt.nlp.num_option('tol', 1e-3)
+opt.nlp.int_option('max_iter', 50)
+opt.nlp.num_option('tol', 1e-8)
 
 opt_sol = opt.solve(cable_positions)
 
 MC.eval_J(opt_sol)
 for i in range(n_cables):
-    print("%.2e, %.2e" %(cable_positions[2*i], cable_positions[2*i+1]))
+    print("%.8f, %.8f" %(opt_sol[2*i], opt_sol[2*i+1]))
         
 for i in range(MC.multimesh.num_parts()):
     outputs[i] << MC.T.part(i)
