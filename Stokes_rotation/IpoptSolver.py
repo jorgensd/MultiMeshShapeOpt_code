4d4fab2cc7535ffb6062199a1e66145b79294109
dokken <dokken92@gmail.com>
2018-10-04 13:39:36 +0200

    Added some print statements and comments

--- Stokes_rotation/IpoptSolver.py
+++ Stokes_rotation/IpoptSolver.py
@@ -49,26 +49,37 @@ class IpoptAngle():
 
 if __name__ == "__main__":
     from StokesSolver import *
-    points = [Point(0.5,0.5)]#[Point(0.5,0.25), Point(0.75,0.52), Point(0.3,0.8)]
-    thetas = numpy.array([0], dtype=float)#[90, 47, 32]
+    points = [Point(0.5,0.25), Point(0.75,0.52), Point(0.3,0.8)]
+    init_angles = [90, 47, 32]
+    thetas = numpy.array([0], dtype=float)
     inlet_str= "-A*(x[1]-x_l)*(x[1]-x_u)"
     inlet_data = [[Expression((inlet_str, "0"), x_l=0.1, x_u=0.4,
-                                     A=250, degree=5), 1],
-                         [Expression((inlet_str, "0"), x_l=0.7, x_u=0.85,
-                                     A=0, degree=5), 2]]
+                              A=250, degree=5), 1],
+                  [Expression((inlet_str, "0"), x_l=0.7, x_u=0.85,
+                              A=120, degree=5), 2]]
 
     pre = "meshes/"
     meshes = [pre+"multimesh_0.xdmf"] +  [pre+"multimesh_1.xdmf"]*len(points)
     mfs = [pre+"mf_0.xdmf"] + [pre+"mf_1.xdmf"]*len(points)
     solver = StokesSolver(points, thetas, meshes, mfs, inlet_data)
-    # Save initial solution
+
+    # Initialize optimizer
     optimizer = IpoptAngle(len(thetas),solver.eval_J, solver.eval_dJ)
     optimizer.nlp.int_option('max_iter',30)
     optimizer.nlp.num_option("tol", 1e-6)
+    # Solve optimization problem
     opt_theta = optimizer.solve(thetas)
 
-    solver.eval_J(thetas)
+    # Save initial solution
+    J_init = solver.eval_J(thetas)
+    print("Initial Theta")
+    print(thetas)
+    print("Initial J: %.2e" % J_init)
     solver.save_state()
-    print(opt_theta, solver.eval_J(opt_theta))
-    # solve_final solution
+
+    # Save final solution
+    print("Optimal Theta")
+    print(opt_theta)
+    J_opt = solver.eval_J(opt_theta)
+    print("Optimal J: %.2e" % J_opt)
     solver.save_state()
