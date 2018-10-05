136163fb6ce05647fbcbffef05e7c97bf0001d96
dokken <dokken92@gmail.com>
2018-10-05 13:38:47 +0200

    Change opt method to Newton-cG with avg xtol quitting criteria

--- Stokes_rotation/scipysolver.py
+++ Stokes_rotation/scipysolver.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
               0.25,0.75,0.5,0.75,0.75,0.75]
     points = [Point(Points[2*i],Points[2*i+1])
               for i in range(int(len(Points)/2))]
+    # points = [Point(0.24,0.5)]
     init_angles = [0 for i in range(len(points))]
     thetas = numpy.array(init_angles, dtype=float)
     inlet_str= "-A*(x[1]-x_l)*(x[1]-x_u)"
@@ -28,20 +29,20 @@ if __name__ == "__main__":
     print(init_angles)
     print("Initial J: %.2e" % J_init)
     solver.save_state()
-    
+
     # Initialize optimizer
     from scipy.optimize import minimize
-    opt_theta = minimize(solver.eval_J, thetas,
+    result = minimize(solver.eval_J, thetas,
                          jac=solver.eval_dJ,
-                         # method = 'Nelder-Mead',
+                         method = 'Newton-CG',
                          callback=solver.callback,
-                         options={"maxiter":100, "disp":True})
-    embed()
+                         options={"maxiter":50,"xtol":1e-2, "disp":True})
 
     # Save final solution
     print("Optimal Theta")
-    dJ_opt = solver.eval_dJ(opt_theta)
-    J_opt = solver.eval_J(opt_theta)
+    print(result["x"])
+    dJ_opt = solver.eval_dJ(result["x"])
+    J_opt = solver.J
     print("Optimal J: %.5f" % J_opt)
     print("Final gradient: ", dJ_opt)
     solver.save_state()
