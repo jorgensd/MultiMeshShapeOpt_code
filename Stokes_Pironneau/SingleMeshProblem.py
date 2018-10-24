81ecafc92ff53a86538066e873150e40a099ca1f
dokken <dokken92@gmail.com>
2018-10-24 13:32:47 +0200

    Started writing a singlemesh solver to see if the problem is neumann conditions

--- Stokes_Pironneau/SingleMeshProblem.py
+++ Stokes_Pironneau/SingleMeshProblem.py
@@ -1,8 +1,11 @@
 from dolfin import (Mesh, MeshFunction, VectorElement, FiniteElement,
                     Function, FunctionSpace, XDMFFile, cpp,
-                    MeshValueCollection, Constant, DirichletBC)
+                    MeshValueCollection, Constant, DirichletBC,
+                    plot, TestFunctions, TrialFunctions,
+                    inner, grad, dx, div)
 from IPython import embed
 from pdb import set_trace
+from matplotlib.pyplot import show
 
 class StokesSolver():
     def __init__(self, mesh, mf, bc_dict):
@@ -25,10 +28,13 @@ class StokesSolver():
             self.bcs.append(bc)
 
     def solve(self):
-        (u, p) = TrialFunctions(VQ)
-        (v, q) = TestFunctions(VQ)
-        
-
+        (u, p) = TrialFunctions(self.VQ)
+        (v, q) = TestFunctions(self.VQ)
+        a = inner(grad(u), grad(v))*dx - div(u)*q*dx - div(v)*p*dx
+        l = inner(f, v)*dx
+        solve(a==l, w, self.bcs, "mumps")
+        plot(w.split()[0])
+        show()
 if __name__ == "__main__":
     mesh = Mesh()
     with XDMFFile("meshes/singlemesh.xdmf") as infile:
@@ -38,5 +44,6 @@ if __name__ == "__main__":
         infile.read(mvc, "name_to_read")
     mf = cpp.mesh.MeshFunctionSizet(mesh, mvc)
     from create_meshes import inflow, outflow, walls
-    StokesSolver(mesh, mf, {inflow: Constant((1.0,0.0)),
-                            walls: Constant((0.0,0.0))})
+    solver = StokesSolver(mesh, mf, {inflow: Constant((1.0,0.0)),
+                                     walls: Constant((0.0,0.0))})
+    solver.solve()
