08480a493378bb5dfb4c169f448c686ec9448086
dokken <dokken92@gmail.com>
2018-10-24 12:49:10 +0200

    added singlemesh mesh creation to create_meshes

--- Stokes_Pironneau/create_meshes.py
+++ Stokes_Pironneau/create_meshes.py
@@ -49,7 +49,10 @@ def background_mesh(res=0.025):
         cell_data={"line": {"name_to_read":
                             cell_data["line"]["gmsh:physical"]}}))
 
-def front_mesh(res=0.025):
+def front_mesh_symmetric(res=0.025):
+    """
+    Creates a donut mesh with symmetry line through y = c_y
+    """
     geometry = Geometry()
     mesh_r = width_scale*res
     c = geometry.add_point((c_x,c_y,0))
@@ -102,6 +105,9 @@ def front_mesh(res=0.025):
                             cell_data["line"]["gmsh:physical"]}}))
 
 def front_mesh_unsym(res=0.025):
+    """
+    Creates unsymmetric donut mesh
+    """
     geometry = Geometry()
     mesh_r = width_scale*res
     c = geometry.add_point((c_x,c_y,0))
@@ -147,6 +153,49 @@ def front_mesh_unsym(res=0.025):
         cell_data={"line": {"name_to_read":
                             cell_data["line"]["gmsh:physical"]}}))
 
+
+
+
+def single_mesh(res=0.025):
+    """ 
+    Creates a single mesh containing a circular obstacle
+    """
+
+    geometry = Geometry()
+    c = geometry.add_point((c_x,c_x,0))    
+
+    # Elliptic obstacle
+    p1 = geometry.add_point((c_x-r_x, c_x,0))
+    p2 = geometry.add_point((c_x, c_x+r_x,0))
+    p3 = geometry.add_point((c_x+r_x, c_x,0))
+    p4 = geometry.add_point((c_x, c_x-r_x,0))
+    arc_1 = geometry.add_ellipse_arc(p1, c, p2, p2)
+    arc_2 = geometry.add_ellipse_arc(p2, c, p3, p3)
+    arc_3 = geometry.add_ellipse_arc(p3, c, p4, p4)
+    arc_4 = geometry.add_ellipse_arc(p4, c, p1, p1)
+    obstacle_loop = geometry.add_line_loop([arc_1, arc_2, arc_3, arc_4])
+
+    rectangle = geometry.add_rectangle(0,L,0,H,0, res, holes=[obstacle_loop])
+    
+    geometry.add_physical_surface(rectangle.surface,label=12)
+    geometry.add_physical_line(obstacle_loop.lines, label=inner_marker)
+    geometry.add_physical_line(rectangle.line_loop.lines, label=outer_marker)
+    field = geometry.add_boundary_layer(edges_list=obstacle_loop.lines,
+                                        hfar=res, hwall_n=res/2, thickness=2*res)
+    geometry.add_background_field([field])
+
+    (points, cells, point_data,
+     cell_data, field_data) = generate_mesh(geometry, prune_z_0=True,
+                                            geo_filename="meshes/singlemesh.geo")
+    
+    meshio.write("meshes/singlemesh.xdmf", meshio.Mesh(
+        points=points, cells={"triangle": cells["triangle"]}))
+    
+    meshio.write("meshes/mf.xdmf", meshio.Mesh(
+        points=points, cells={"line": cells["line"]},
+        cell_data={"line": {"name_to_read":
+                            cell_data["line"]["gmsh:physical"]}}))
+
         
 if __name__=="__main__":
     import sys
@@ -155,5 +204,6 @@ if __name__=="__main__":
     except IndexError:
         res = 0.01
     background_mesh(res)
-    # front_mesh(res)
+    # front_mesh_symmetric(res)
     front_mesh_unsym(res)
+    single_mesh(res)
