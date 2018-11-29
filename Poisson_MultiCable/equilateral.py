6ddbc3e3ab1a130681053befadcb9c0cbd8c92e6
jorgensd <dokken92@gmail.com>
2018-11-29 11:46:02 +0100

    Plotting for three cables are done

--- Poisson_MultiCable/equilateral.py
+++ Poisson_MultiCable/equilateral.py
@@ -31,7 +31,7 @@ cable_positions = numpy.array([c1[0],c1[1],c2[0],c2[1],c3[0],c3[1]])
 compute_angles(cable_positions)
 
 scales = numpy.array([1,1,1])   
-sources = numpy.array([1,1,1])
+sources = numpy.array([10,10,10])
 
 from MultiCable import *
 from IpoptMultiCableSolver import *
@@ -51,12 +51,31 @@ opt = MultiCableOptimization(3, scales, MC.eval_J, MC.eval_dJ)
 opt.nlp.int_option('max_iter', 30)
 opt.nlp.num_option('tol', 1e-6)
 
+opt_sol = opt.solve(cable_positions)
 
-def plot_cable(positions):
-    MC.eval_J(positions)
-    fig = plt.figure()
-    ax = fig.gca()
+compute_angles(opt_sol)
+MC.eval_J(opt_sol)
+
+def plot_init_opt(init, opt,filename):
+    plt.subplot(1,2,1)
+    MC.eval_J(init)
     # Strip T of inactive dofs values
+    N = 512 # Number of levels used in contour plot
+    
+    plot_cable(init,N)
+
+    plt.subplot(1,2,2)
+    MC.eval_J(opt)
+    
+    plot_cable(opt,N)
+    plt.savefig(filename, pad_inches=0, dpi=250,
+                bboxinches="tight")
+    import os
+    os.system("convert " + filename + " -trim " + filename)
+    
+def plot_cable(positions,N):
+    fig = plt.gcf()
+    ax = fig.gca()
     T_arr = MC.T.vector().get_local()
     inactive = MC.T.function_space().dofmap().inactive_dofs(MC.multimesh, 0)
     T_stripped = [val for idx, val in enumerate(T_arr)
@@ -64,7 +83,7 @@ def plot_cable(positions):
     T_min = numpy.min(T_stripped)
     T_max = numpy.max(T_stripped)
     norm = mpl.colors.Normalize(vmin=T_min, vmax=T_max)
-    N = 512 # Number of levels used in contour plot
+    
     p0 = plot(MC.T.part(0),norm=norm,zorder=0,
               levels=numpy.linspace(T_min,T_max,N))
 
@@ -74,29 +93,21 @@ def plot_cable(positions):
         metal_radius = 2./3*opt.inner_radius[i-1]
         iso_circle = plt.Circle((positions[2*(i-1)], positions[2*(i-1)+1]),
                             radius=rubber_radius, color="k",
-                            fill=False,zorder=10+i)
+                                fill=False,zorder=10+i,linewidth=0.5)
         metal_circle = plt.Circle((positions[2*(i-1)], positions[2*(i-1)+1]),
-                            radius=metal_radius, color="k",
-                            fill=False,zorder=10+i)
+                                  radius=metal_radius, color="k",
+                                  fill=False,zorder=10+i,linewidth=0.5)
 
         ax.add_patch(iso_circle)
         ax.add_patch(metal_circle)
-    ax = fig.gca()
-    # Creating scaled colorbar from stripped data
     m = plt.cm.ScalarMappable()
     m.set_array(T_stripped)
-    cbar = plt.colorbar(m,boundaries=numpy.linspace(T_min,T_max,N))
+    cbar = plt.colorbar(m,orientation="horizontal",
+                        ticks=numpy.linspace(T_min,T_max, 4),
+                        boundaries=numpy.linspace(T_min,T_max,N),pad=0.01,
+                        format="%.2f")
+
     cbar.set_clim(T_min,T_max)
-    plt.savefig("output/init_cable.png")
-    exit(1)
+    plt.axis("off")
 
-plot_cable(cable_positions)
-exit(1)
-
-opt_sol = opt.solve(cable_positions)
-compute_angles(opt_sol)
-MC.eval_J(opt_sol)
-
-# for i in range(MC.multimesh.num_parts()):
-#     outputs[i] << MC.T.part(i)
-
+plot_init_opt(cable_positions, opt_sol,"output/equilateral.png")
