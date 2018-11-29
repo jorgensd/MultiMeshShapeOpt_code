34328e56701081f68c2fa828459867ce96e7d805
jorgensd <dokken92@gmail.com>
2018-11-29 17:34:39 +0100

    added plotting for taylor-tests

---
+++ Poisson_MultiCable/plot_taylor.py
@@ -0,0 +1,55 @@
+from IPython import embed
+import matplotlib.pyplot as plt
+from os import system
+
+def plot_rates_1(lines, i, color):
+    epsilons = [float(k) for k in lines[i+1].split()]
+    res_0 = [float(k) for k in lines[i+2].split()]
+    res_1 = [float(k) for k in lines[i+3].split()]
+    rate_0 = [float(k) for k in lines[i+4].split()]
+    rate_1 = [float(k) for k in lines[i+5].split()]
+    # plt.semilogx(epsilons[1:], rate_0,color=color, linestyle="dashed")
+    plt.semilogx(epsilons[1:], rate_1,color=color, linestyle="solid",
+                 marker="o",label="# Cells: %s" %lines[i].split()[-1])
+
+def plot_rates_0(lines, i, color):
+    epsilons = [float(k) for k in lines[i+1].split()]
+    res_0 = [float(k) for k in lines[i+2].split()]
+    res_1 = [float(k) for k in lines[i+3].split()]
+    rate_0 = [float(k) for k in lines[i+4].split()]
+    rate_1 = [float(k) for k in lines[i+5].split()]
+    plt.semilogx(epsilons[1:], rate_0,color=color, linestyle="solid",
+                 marker="o",label="# Cells: %s" %lines[i].split()[-1])
+
+
+
+
+filename = "output/taylor_data.txt"
+
+i = 0
+for res in [2,3,4,5,6,7]:
+    if i == 0:
+        suffix = ">"
+    else:
+        suffix = ">>"
+    system("python3 refine_mesh.py {0:1d}".format(res))
+    system("python3 taylor_test.py" + suffix + filename)
+    i+=1
+
+taylor_data = open(filename, "r")
+lines = taylor_data.readlines()
+indicies = [i for i, s in enumerate(lines) if '#Cells' in s]
+colors = ["r","b", "g","k","c","m"]
+counter=0
+plt.figure()
+for i in indicies:
+    plot_rates(lines,i,colors[counter])
+    counter+=1
+ax = plt.gca()
+ax.legend()
+plt.grid(True)
+plt.ylim((0,4))
+plt.xlabel(r"Perturbation length ($\epsilon$)")
+plt.ylabel(r"Convergence rate")
+plt.savefig("output/taylor_test.png")
+
--- Poisson_MultiCable/taylor_test.py
+++ Poisson_MultiCable/taylor_test.py
@@ -9,16 +9,12 @@ def convergence_rates(E_values, eps_values):
                  numpy.log(eps_values[i]/eps_values[i-1]))
     return r
 
-
 lmb_metal = 205.   # Heat coefficient aluminium
-lmb_insulation = 0.2 # Heat coefficient of plastic
+lmb_insulation = 0.33 # Heat coefficient of plastic
 lmb_air = 0.33   # Heat coefficient of brick
-c1 = numpy.array([0, 0.1])
-
-
-scales = numpy.array([1])   
-sources = numpy.array([25])
-
+c1 = numpy.array([0, 0.2,-0.2,-0.4])
+scales = numpy.array([1,1])   
+sources = numpy.array([25,25])
 from MultiCable import *
 
 MC = MultiCable(scales, c1, lmb_metal, lmb_insulation,
@@ -29,23 +25,29 @@ for i in range(MC.multimesh.num_parts()):
 
 from dolfin import plot, File
 
-perturbation= numpy.array([0,0.75])
+perturbation= numpy.array([0,0.65,-0.1,0.2])
 dJ = MC.eval_dJ(c1)
 dJp = numpy.dot(dJ, perturbation)
-print(MC.J,dJ)
 J = MC.J
 
-epsilon = [0.5**(i) for i in range(12)]
+epsilon = [0.5**(i) for i in range(10)]
 res_0 = []
 res_1 = []
+outputs = [File("output/taylor%d.pvd" %i)
+           for i in range(MC.multimesh.num_parts())]
+for i in range(MC.multimesh.num_parts()):
+    outputs[i] << MC.T.part(i)
 for eps in epsilon:
     J_eps = MC.eval_J(c1+eps*perturbation)
     res_0.append(numpy.abs(J_eps-J))
     res_1.append(numpy.abs(J_eps-J-eps*dJp))
+    for i in range(MC.multimesh.num_parts()):
+        outputs[i] << MC.T.part(i)
 
 print("#Cells", num_cells)
-print(' '.join('{:1.2e}'.format(k) for k in epsilon))
-print(' '.join('{:1.2e}'.format(k) for k in res_0))
-print(' '.join('{:1.2e}'.format(k) for k in res_1))
-print(' '.join('{:1.2e}'.format(k) for k in convergence_rates(res_0,epsilon)))
-print(' '.join('{:1.2e}'.format(k) for k in convergence_rates(res_1,epsilon)))
+print(' '.join('{:1.5e}'.format(k) for k in epsilon))
+print(' '.join('{:1.5e}'.format(k) for k in res_0))
+print(' '.join('{:1.5e}'.format(k) for k in res_1))
+print(' '.join('{:1.5e}'.format(k) for k in convergence_rates(res_0,epsilon)))
+print(' '.join('{:1.5e}'.format(k) for k in convergence_rates(res_1,epsilon)))
+
