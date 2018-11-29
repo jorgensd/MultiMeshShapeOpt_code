7608edfcfc6321f9b067ca36b5d2b5091aaaaebf
jorgensd <dokken92@gmail.com>
2018-11-29 16:07:21 +0100

    UPdated taylor-test to pretty print and makefile

--- Poisson_MultiCable/taylor_test.py
+++ Poisson_MultiCable/taylor_test.py
@@ -10,46 +10,42 @@ def convergence_rates(E_values, eps_values):
     return r
 
 
-lmb_metal = 210.      # Heat coefficient aluminium
-lmb_insulation = 0.44 # Heat coefficient of plastic
-lmb_air = 0.03   # Heat coefficient of brick
-c1 = numpy.array([0.02, 0.1])
+lmb_metal = 205.   # Heat coefficient aluminium
+lmb_insulation = 0.2 # Heat coefficient of plastic
+lmb_air = 0.33   # Heat coefficient of brick
+c1 = numpy.array([0, 0.1])
 
 
 scales = numpy.array([1])   
-sources = numpy.array([10])
+sources = numpy.array([25])
 
 from MultiCable import *
 
 MC = MultiCable(scales, c1, lmb_metal, lmb_insulation,
                       lmb_air, sources)
+num_cells = 0 
+for i in range(MC.multimesh.num_parts()):
+    num_cells += MC.multimesh.part(i).num_cells()
 
 from dolfin import plot, File
-outputs = [File("output/taylor%d.pvd" %i)
-           for i in range(MC.multimesh.num_parts())]
 
-perturbation= numpy.array([0.5,0.5])
+perturbation= numpy.array([0,0.75])
 dJ = MC.eval_dJ(c1)
 dJp = numpy.dot(dJ, perturbation)
-
+print(MC.J,dJ)
 J = MC.J
-for i in range(MC.multimesh.num_parts()):
-    outputs[i] << MC.T.part(i)
 
-epsilon = [0.5**(i) for i in range(5)]
+epsilon = [0.5**(i) for i in range(12)]
 res_0 = []
 res_1 = []
 for eps in epsilon:
     J_eps = MC.eval_J(c1+eps*perturbation)
-    for i in range(MC.multimesh.num_parts()):
-        outputs[i] << MC.T.part(i)
     res_0.append(numpy.abs(J_eps-J))
     res_1.append(numpy.abs(J_eps-J-eps*dJp))
-print("FD residuals")
-print(["%.2e" % i for i in res_0])
-print("FD rate")
-print(["%.2f" % i for i in convergence_rates(res_0, epsilon)])
-print("1st order residuals")
-print(["%.2e" % i for i in res_1])
-print("1st order rate")
-print(["%.2f" % i for i in convergence_rates(res_1, epsilon)])
+
+print("#Cells", num_cells)
+print(' '.join('{:1.2e}'.format(k) for k in epsilon))
+print(' '.join('{:1.2e}'.format(k) for k in res_0))
+print(' '.join('{:1.2e}'.format(k) for k in res_1))
+print(' '.join('{:1.2e}'.format(k) for k in convergence_rates(res_0,epsilon)))
+print(' '.join('{:1.2e}'.format(k) for k in convergence_rates(res_1,epsilon)))
